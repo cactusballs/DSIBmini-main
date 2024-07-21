607f66d18f28f772ca9af04ef45fdf6f770934f1
cactusballs <71734928+cactusballs@users.noreply.github.com>
2024-07-21 07:25:08 +0100

    adding a new array item

--- DSIBmini.js
+++ DSIBmini.js
@@ -36,13 +36,15 @@ function generateAnswer() {
            'Oh wow, you bought a reusable coffee cup and shopping bag? but you\'re still smoking? Look up the environmental impacts <a href="https://www.who.int/news/item/31-05-2022-who-raises-alarm-on-tobacco-industry-environmental-impact" target="_blank">here</a> ya fanny.<br>Don\'t smoke, it\'s bad',
            'People love you and you will be loved throughout life, don\'t leave them early or make them watch you die a really painful death.<br>Don\'t smoke, it\'s bad',
            'Just between us, we can admit we\'re vein and with good reason, you\'re a hotty but smoking will steal your looks like a Disney villain. You\'re gonna look old, haggard, have yellow teeth and baggy, baggy skin. Quit smoking, stay hot.<br>Don\'t smoke, it\'s bad',
-           'Smoking is paying to die a painful death. You could spend that money on a daily iced latte and croissant.<br>Don\'t smoke, it\'s bad'
+           'Smoking is paying to die a painful death. You could spend that money on a daily iced latte and croissant.<br>Don\'t smoke, it\'s bad', 
+           'The people who make cigarettes are profiting from your slow painful death. Maybe don\'t let them?.<br>Don\'t smoke, it\'s bad'
+
   ];
   response = responses[Math.floor(Math.random() * responses.length)];
   document.getElementById('outputDiv').innerHTML = response;
   eightBall.innerHTML = 'Press me again?';
   eightBall.style.color = 'green';
-} 
+};
 
 
 
